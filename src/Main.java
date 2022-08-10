11ed991581c4762f096c936fcb25c67291cd7943
GolubevAndrey <golubev_andrei87@mail.ru>
2022-08-10 13:19:55 +0300

    Выполненное ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-//        Задание 1
+        Author Garcia = new Author("Габриэль", "Гарсиа Маркес");
+        Author Conan = new Author("Артур Конан", "Дойл");
+        Book soledad = new Book(Garcia.getFistName() + " " + Garcia.getLastName(), "Сто лет одиночества", 1967);
+        Book WhiteCompany = new Book(Conan.getFistName() + " " + Conan.getLastName(), "Белый отряд", 1891);
+        System.out.println("soledad = " + soledad.getBookName() + " " + soledad.getAuthorName() + " " + soledad.getBookYear());
+        System.out.println("WhiteCompany = " + WhiteCompany.getBookName() + " " + WhiteCompany.getAuthorName() + " " + WhiteCompany.getBookYear());
+        soledad.setBookYear(1984);
+        System.out.println("soledad = " + soledad.getBookName() + " " + soledad.getAuthorName() + " " + soledad.getBookYear());
     }
 }
